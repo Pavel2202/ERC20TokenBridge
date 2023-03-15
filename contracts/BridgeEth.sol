2eb7354e43d4957d3db0029182e250fb9fa835db
Pavel2202 <paveltimenov3@gmail.com>
2023-03-15 13:20:11 +0200

    Add Bridges for Eth and Matic

---
+++ contracts/BridgeEth.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "./BridgeBase.sol";
+
+contract BridgeEth is BridgeBase {
+    constructor(address token) BridgeBase(token) {}
+}
---
+++ contracts/BridgeMatic.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "./BridgeBase.sol";
+
+contract BridgeMatic is BridgeBase {
+    constructor(address token) BridgeBase(token) {}
+}
