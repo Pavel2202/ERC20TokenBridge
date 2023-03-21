734d7f930478add35f66c0216f56b0dceaf4f4be
Pavel2202 <paveltimenov3@gmail.com>
2023-03-21 13:31:21 +0200

    Add event listener to burn function that executes mint

--- erc20-token-bridge-ui/components/Main.js
+++ erc20-token-bridge-ui/components/Main.js
@@ -1,21 +1,22 @@
 import { ethers } from "ethers";
 import { contractAddresses, abi } from "../constants/EthBridge";
 import { useMoralis, useWeb3Contract } from "react-moralis";
+import { useEffect, useState } from "react";
 
 const Main = () => {
+  const [provider, setProvider] = useState();
+  const [params, setParams] = useState({});
+
+  useEffect(() => {
+    setProvider(new ethers.providers.Web3Provider(window.ethereum));
+  }, []);
+
   const { Moralis, isWeb3Enabled, chainId: chainIdHex } = useMoralis();
   const chainId = parseInt(chainIdHex);
   const ethBridgeAddress =
     chainId in contractAddresses ? contractAddresses[chainId] : null;
 
   const addTokenValue = 5000000000;
-
-  const { runContractFunction: balance } = useWeb3Contract({
-    abi: abi,
-    contractAddress: ethBridgeAddress,
-    functionName: "getBalance",
-    params: {},
-  });
 
   const { runContractFunction: burn } = useWeb3Contract({
     abi: abi,
@@ -32,21 +33,35 @@ const Main = () => {
     abi: abi,
     contractAddress: ethBridgeAddress,
     functionName: "mint",
-    params: {
-      from: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-      to: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-      amount: addTokenValue,
-      otherChainNonce: 0,
-    },
+    params: params,
   });
 
-  async function getBalance() {
-    console.log("test");
-    await burn();
-    await mint();
+  async function listenToEvent() {
+    const ethBridge = await new ethers.Contract(
+      ethBridgeAddress,
+      abi,
+      provider
+    );
+
+    ethBridge.once("Transfer", async (from, to, amount, date, nonce, step) => {
+      setParams((params) => ({
+        from: from,
+        to: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+        amount: Number(amount),
+        otherChainNonce: Number(nonce),
+      }));
+      console.log(params);
+      await mint();
+    });
   }
 
-  return <button onClick={getBalance}>GetBalance</button>;
+  async function burnFunction() {
+    console.log("burn");
+    await burn();
+    listenToEvent();
+  }
+
+  return <button onClick={burnFunction}>GetBalance</button>;
 };
 
 export default Main;
