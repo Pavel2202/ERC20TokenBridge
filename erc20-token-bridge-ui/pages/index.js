3d145483b6f0c5a9ab8dacf220d5263eb8a76b4b
Pavel2202 <paveltimenov3@gmail.com>
2023-04-11 16:42:48 +0300

    UI initial create

---
+++ erc20-token-bridge-ui/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ERC20 Token Bridge</title>
+        <meta name="description" content="ERC20 Token Bridge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>Hello</div>
+      </main>
+    </>
+  )
+}
